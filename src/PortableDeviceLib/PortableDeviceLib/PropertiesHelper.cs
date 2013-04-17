48624884164ebe071f085325a6e477f394435387
Rostislav Chekan <r.chekan@gmail.com>
2013-04-17 17:50:41 +0300

    Rework property handling

--- src/PortableDeviceLib/PortableDeviceLib/PropertiesHelper.cs
+++ src/PortableDeviceLib/PortableDeviceLib/PropertiesHelper.cs
@@ -13,31 +13,31 @@ namespace PortableDeviceLib
             this.device = device;
         }
 
-        public T GetProperty<T>(string objectId, _tagpropertykey propertyKey, Func<IPortableDeviceValues, T> extractor)
+        public IPortableDeviceProperties GetProperties()
         {
             //Ensure we are connected to device
             CheckIfIsConnected();
 
             IPortableDeviceContent content;
             IPortableDeviceProperties properties;
-            IPortableDeviceValues propertyValues;
 
             device.PortableDeviceClass.Content(out content);
             content.Properties(out properties);
+            
+            return properties;
+        }
 
-            properties.GetValues(objectId, null, out propertyValues);
+        private T GetPropertyValues<T>(string objectId, Func<IPortableDeviceValues, T> extractor)
+        {
+            IPortableDeviceValues propertyValues;
+            GetProperties().GetValues(objectId, null, out propertyValues);
 
             return extractor(propertyValues);
         }
 
-        public T GetProperty<T>(PortableDeviceObject obj, _tagpropertykey propertyKey, Func<IPortableDeviceValues, T> extractor)
-        {
-            return GetProperty(obj.ID, propertyKey, extractor);
-        }
-
         public string GetStringProperty(string objectId, _tagpropertykey propertyKey)
         {
-            return GetProperty(objectId, propertyKey, deviceValues =>
+            return GetPropertyValues(objectId, deviceValues =>
                 {
                     string val;
                     deviceValues.GetStringValue(ref propertyKey, out val);
@@ -47,7 +47,7 @@ namespace PortableDeviceLib
 
         public int GetIntegerProperty(string objectId, _tagpropertykey propertyKey)
         {
-            return GetProperty(objectId, propertyKey, deviceValues =>
+            return GetPropertyValues(objectId, deviceValues =>
                 {
                     int val;
                     deviceValues.GetSignedIntegerValue(ref propertyKey, out val);
@@ -57,7 +57,7 @@ namespace PortableDeviceLib
 
         public bool GetBooleanProperty(string objectId, _tagpropertykey propertyKey)
         {
-            return GetProperty(objectId, propertyKey, deviceValues =>
+            return GetPropertyValues(objectId, deviceValues =>
                 {
                     int val;
                     deviceValues.GetBoolValue(ref propertyKey, out val);
